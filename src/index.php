05d635514d5995d9f7f2bf50cdbc59f999c5c475
memesmaskmm@gmail.com <memesmaskmm@gmail.com>
2025-08-31 14:40:56 +0200

    erster Commit

---
+++ src/index.php
@@ -0,0 +1,17 @@
+<html lang="de">
+<body>
+<h1>Mein Blog </h1>
+<label> Suche: <input type="search" name="suche" placeholder="suchen"/> </label>
+<button>Suche starten</button>
+<br><br>
+<h3>Eintrag verfassen</h3>
+<label> Titel: <input type="text" name="titel" placeholder="Blog-Titel"/> </label>
+<label> <input type="date" name="datum"/></label>
+<label> <input type="time" name="uhrzeit"/></label>
+<br><br>
+<label> Eintrag: <textarea name="blogeintrag"> </textarea> </label>
+<button name="speichern">Speichern</button>
+<h3>alle Einträge</h3>
+<button type="button">filtern nach</button>
+</body>
+</html>
